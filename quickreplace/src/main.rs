85e8cb5e2d8b777207dc0f87429e471bd201be04
Manuel Reyes <Manueldjsr@gmail.com>
2024-11-18 00:45:58 -0400

    Finished chapter: read and write to fs

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,16 +1,21 @@
-use text_colorizer::*;
+use regex::Regex;
 use std::env; // env variables
+use std::fs;
+use text_colorizer::*;
 
 #[derive(Debug)]
 struct Arguments {
     target: String,
     replacement: String,
     filename: String,
-    output: String
+    output: String,
 }
 
-fn print_usage(){
-    eprintln!("{} - change occurrences of one string into another", "quickreplace".green());
+fn print_usage() {
+    eprintln!(
+        "{} - change occurrences of one string into another",
+        "quickreplace".green()
+    );
     eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
 }
 
@@ -19,7 +24,11 @@ fn parse_args() -> Arguments {
 
     if args.len() != 4 {
         print_usage();
-        eprintln!("{} wrong number of arguments: expected 4, got {}", "Error:".red().bold(), args.len());
+        eprintln!(
+            "{} wrong number of arguments: expected 4, got {}",
+            "Error:".red().bold(),
+            args.len()
+        );
         std::process::exit(1);
     }
 
@@ -27,11 +36,38 @@ fn parse_args() -> Arguments {
         target: args[0].clone(),
         replacement: args[1].clone(),
         filename: args[2].clone(),
-        output: args[3].clone()
+        output: args[3].clone(),
     }
+}
+
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
 }
 
 fn main() {
     let args = parse_args();
-    println!("{:?}", args);
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprint!("Failed to read from []");
+            std::process::exit(1);
+        }
+    };
+
+    let replace_data = match replace(&args.target, &args.replacement, &data){
+        Ok(v) => v,
+        Err(e) => {
+            eprint!("Failed to replace text []");
+            std::process::exit(1);
+        }
+    }
+
+    match fs::write(&args.output, &replace_data) {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("[] failed to write file");
+            std::process::exit(1);
+        }
+    }
 }
