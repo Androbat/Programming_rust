f9b0f2a241afebda16444025a2cf9fd97ac6c8c3
Manuel Reyes <Manueldjsr@gmail.com>
2024-11-19 00:39:13 -0400

    Must fix several issues.

---
+++ types/src/main.rs
@@ -0,0 +1,10 @@
+fn build_vector() -> Vec<i16> {
+    let mut v: Vec<i16> = Vec::<i16>::new();
+    v.push(10i16); // 10 as i16
+    v.push(20i16); // 
+    v
+}
+
+fn main() {
+    println!("Hello, world!");
+}
